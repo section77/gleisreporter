c0669bb96dd01a1af6bfc8bf8cff152309b9c60c
Jan Alexander <jan@nalx.net>
2020-09-16 09:38:05 +0200

    initial commit serving same infos as current static file

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use spaceapi_server::api::{StatusBuilder, Location, Contact, Feeds, Feed, IssueReportChannel};
+use spaceapi_server::api::sensors::PeopleNowPresentSensorTemplate;
+use spaceapi_server::SpaceapiServerBuilder;
+
+fn main() {
+    let status = StatusBuilder::new("section77")
+        .logo("https://section77.de/static/section77_logo_vector.svg")
+        .url("https://section77.de/")
+        .location(Location {
+            address: Some("Hauptstraße 1, 77652 Offenburg, Germany".into()),
+            lat: 48.4771,
+            lon: 7.9461,
+        })
+        .contact(Contact {
+            email: Some("info@section77.de".into()),
+            twitter: Some("@section77de".into()),
+            ..Default::default()
+        })
+        .feeds(Feeds {
+            calendar: Some(Feed {
+                type_: Some("ical".into()),
+                url: "https://section77.de/section77.ics".to_string(),
+            }.into()),
+            ..Default::default()
+        })
+        .add_issue_report_channel(IssueReportChannel::Email)
+        .build()
+        .expect("Creating status failed");
+
+    let server = SpaceapiServerBuilder::new(status)
+        .redis_connection_info("redis://127.0.0.1/")
+        .add_sensor(PeopleNowPresentSensorTemplate {
+            location: None,
+            name: None,
+            description: None,
+            names: None,
+        }, "people_now_present".into())
+        .build()
+        .unwrap();
+
+    let _ = server.serve("0.0.0.0:8000");
+}
